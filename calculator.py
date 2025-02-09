d3f306ae7286274dc6fa40af7b2778287a5cb5d1
Preetam Saha <rinkusaha794@gmail.com>
2025-02-09 14:03:55 +0530

    Added divide function

--- calculator.py
+++ calculator.py
@@ -4,6 +4,11 @@ def add(a,b) :
     return a+b
 def subtract(a,b) :
     return a-b
+def divide(a,b) :
+    if b==0 :
+        print("Error: division by zero is not allowed.")
+        return None
+    return a/b
 if __name__ == "__main__":
     print("Simple Calculator")
     x=10
@@ -11,5 +16,5 @@ if __name__ == "__main__":
     print("Add:", add(x,y) )
     print("Subtract:", subtract(x,y))
     print("Multiply:", multiply(x,y))
-
+    print("Divide:", divide(x,y))
 
